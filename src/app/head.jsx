743787e2d1a76618d536ffd0dc8870096f23443d
Caiquegam <caiquegamadeoliveira@gmail.com>
2023-10-26 16:49:41 -0300

    install next js

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb Clone</title>
+      <meta content="width-device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="IMDb clone" />
+      <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+    </>
+  )
+}
+
