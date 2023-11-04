c79dc7432c91116f934e6ffbab9bd93716270fc3
Caiquegam <caiquegamadeoliveira@gmail.com>
2023-11-04 18:07:27 -0300

    fetch data from tmdb api in the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,28 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Results from '@/components/Results'
 
-const inter = Inter({subsets: ["latin"]});
+const API_KEY = process.env.API_KEY
 
-
-export default function Home() {
-  return (
-    <h1 className="text-red-800">
-      Hello world!
-    </h1>
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}&lenguage=pt-BR&page=1
+`,
+    { next: { revalidate: 3600 } }
   )
+
+  if (!res.ok) {
+    throw new Error('Falha ao buscar dados!')
+  }
+
+  const data = await res.json()
+
+  const results = data.results
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
